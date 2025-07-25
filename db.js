c1e088ae6e4771d100c1eefc79b44b1e0f999fa8
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-25 11:47:18 +0200

    feat: passed callback function to connectToDB function

--- db.js
+++ db.js
@@ -3,12 +3,21 @@
 // get mongo client
 const uri = "mongodb://localhost:27017/bookstore";
 const {MongoClient} = require("mongodb");
+
+//connect to my local database
 let dbConnection
-
-const connectToDB = async ()=>{
-  //connect to my local database
-  const client = await MongoClient.connect(uri);
-   dbConnection = client.db();
-
-
+const connectToDB = async (cb)=>{
+  let client
+  try{
+    client = await MongoClient.connect(uri);
+    dbConnection = client.db();
+    return cb();
+  }catch(err){
+    console.error("Unable to connect: ",err);
+    return cb(err);
+  }
 }
+
+const getDB = ()=> dbConnection
+
+module.exports = {connectToDB, getDB}
