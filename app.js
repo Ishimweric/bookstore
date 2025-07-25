b9c5bdd24d6e60b2d021b2e2fa9259b0ae351d9d
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-25 11:04:00 +0200

    add: Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+// INITIALISE APP
+const app = express();
+
+// ROUTES FOR REQUEST HANDLIND
+
+
+// LISTEN TO PORT 3500
+app.listen(3500, ()=>{
+  console.log("SERVER RUNNING AT PORT 3500");
+});
