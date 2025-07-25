608bfbf5f03aed35c946098c8f0c23b64a731d7c
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-25 11:07:15 +0200

    add: Added books router

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const app = express();
 
 // ROUTES FOR REQUEST HANDLIND
-
+app.get("/books",(req, res)=>{
+  res.json({"message" : "Welcome to api land"})
+})
 
 // LISTEN TO PORT 3500
 app.listen(3500, ()=>{
